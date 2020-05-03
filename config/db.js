6767ad44a68c56c25b6a152e282b826f8499d604
abdulahiroble <abdulahi_2610@hotmail.com>
2020-05-03 16:30:06 +0200

    name, email and password validation implemented

--- config/db.js
+++ config/db.js
@@ -6,9 +6,8 @@ const db = config.get("mongoURI");
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
+      useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     });
 
     console.log("MongoDB Connected");
