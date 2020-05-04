062d52e9d5329a4f4935574ae09ed3a942f04dcf
abdulahiroble <abdulahi_2610@hotmail.com>
2020-05-04 16:54:34 +0200

    implemented contact model and get contacts route

--- models/User.js
+++ models/User.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    requited: true,
+    required: true,
   },
   email: {
     type: String,
-    requited: true,
+    required: true,
     unique: true,
   },
   password: {
     type: String,
-    requited: true,
+    required: true,
   },
   date: {
     type: Date,
